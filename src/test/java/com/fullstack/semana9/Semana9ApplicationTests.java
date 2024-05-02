fc40c378105c242b911ef72c73be40d58421ecb6
Joao Victor <jvso@yahoo.com>
2024-05-02 10:57:15 -0300

    first commit

---
+++ src/test/java/com/fullstack/semana9/Semana9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.semana9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Semana9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
